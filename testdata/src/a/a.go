8df3dcc5ce30ecb00feec0aea0a978f14ddad824
tenntenn <uedatakuya@gmail.com>
2021-09-08 21:35:09 +0900

    Fix bugs

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -2,17 +2,25 @@ package a
 
 func f() {
 	go func() { // want "this goroutine does not recover a panic"
+		print("hello")
 	}()
 
 	//go func() { // -want "this goroutine does not recover a panic"
 	//	defer recover() // NG
+	//	print("hello")
 	//}()
 
 	go func() { // OK
 		defer func() { recover() }()
+		print("hello")
 	}()
 
 	go func() { // want "this goroutine does not recover a panic"
 		recover()
+		print("hello")
+	}()
+
+	go func() { // want "this goroutine does not recover a panic"
+		defer func() { print("hello") }()
 	}()
 }
--- unrecover.go
+++ unrecover.go
@@ -32,7 +32,7 @@ func run(pass *analysis.Pass) (interface{}, error) {
 		}
 
 		f := callee(g, goinstr)
-		if len(f.Blocks) != 0 && f.Recover == nil {
+		if noRecover(g, f) {
 			pass.Reportf(goinstr.Pos(), "this goroutine does not recover a panic")
 		}
 
@@ -54,4 +54,56 @@ func callee(g *callgraph.Graph, call ssa.CallInstruction) *ssa.Function {
 	}
 
 	return nil
+}
+
+func noRecover(g *callgraph.Graph, f *ssa.Function) bool {
+	if f == nil || len(f.Blocks) == 0 {
+		return false
+	}
+
+	var recovered bool
+	analysisutil.InspectInstr(f.Blocks[0], 0, func(i int, instr ssa.Instruction) bool {
+		deferInstr, _ := instr.(*ssa.Defer)
+		if deferInstr == nil {
+			return true
+		}
+
+		f := callee(g, deferInstr)
+		if !hasRecover(f) {
+			return true
+		}
+
+		recovered = true
+		return false
+	})
+
+	return !recovered
+}
+
+func hasRecover(f *ssa.Function) bool {
+	if f == nil || len(f.Blocks) == 0 {
+		return false
+	}
+
+	var recovered bool
+	analysisutil.InspectInstr(f.Blocks[0], 0, func(i int, instr ssa.Instruction) bool {
+		call, _ := instr.(*ssa.Call)
+		if call == nil {
+			return true
+		}
+
+		builtin, _ := call.Common().Value.(*ssa.Builtin)
+		if builtin == nil {
+			return true
+		}
+
+		if builtin.Name() != "recover" {
+			return true
+		}
+
+		recovered = true
+		return false
+	})
+
+	return recovered
 }
